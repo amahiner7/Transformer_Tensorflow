1aa6c93665563fb2a0dcd383adcb31650aa507cf
amahiner7 <amahiner@gmail.com>
2021-10-15 16:17:15 +0900

    Positional Encoding 구현

---
+++ model/ver1/embedding/PositionalEncoding.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Layer, LayerNormalization, Dropout
+
+
+class PositionalEncoding(Layer):
+    """
+    compute sinusoid encoding.
+    """
+
+    def __init__(self, d_embed, max_len):
+        """
+        constructor of sinusoid encoding class
+
+        :param d_embed: dimension of embedding
+        :param max_len: max sequence length
+        """
+        super().__init__()
+
+        def _get_angle(position, dim, d_embed):
+            return position / np.power(10000, 2 * (dim // 2) / d_embed)
+
+        def _get_positional_angle(position, d_embed):
+            return [_get_angle(position, dim, d_embed) for dim in range(d_embed)]
+
+        # same size with input matrix (for adding with input matrix)
+        self.encoding = np.array(
+            [_get_positional_angle(position=position, d_embed=d_embed) for position in range(max_len)],
+            dtype=np.float32)
+
+        self.encoding[:, 0::2] = np.sin(self.encoding[:, 0::2])
+        self.encoding[:, 1::2] = np.cos(self.encoding[:, 1::2])
+        self.encoding = tf.cast(self.encoding, dtype=tf.float32)
+
+    def call(self, input):
+        """
+        :param input:
+        :return:
+        """
+        position_encoding = input + self.encoding[:tf.shape(input)[1]]
+
+        return position_encoding
+
