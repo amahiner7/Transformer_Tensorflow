6d5f9c613bd50c085cde0407149b77dd471e6ad2
amahiner7 <amahiner@gmail.com>
2021-10-15 16:35:20 +0900

    Transformer Embedding 구현

---
+++ model/ver1/embedding/TransformerEmbedding.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dropout, Embedding
+from model.ver1.embedding.PositionalEncoding import PositionalEncoding
+
+
+class TransformerEmbedding(Layer):
+    def __init__(self, vocab_size, d_model, seq_len, dropout_prob, device):
+        super().__init__()
+
+        self.device = device
+        self.token_embedding = Embedding(input_dim=vocab_size, output_dim=d_model)
+        self.position_embedding = PositionalEncoding(d_model, seq_len, device)
+        self.drop_out = Dropout(rate=dropout_prob)
+        self.scale = tf.math.sqrt(tf.cast(d_model, tf.float32))
+
+    def forward(self, input):
+        token_embedding = self.token_embedding(input)
+        position_embedding = self.position_embedding(input)
+        output = (token_embedding * self.scale) + position_embedding
+        output = self.drop_out(output)
+
+        return output
