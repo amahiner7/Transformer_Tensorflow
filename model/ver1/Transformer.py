e085a4a8f564b62043f7cbd75209ec938fcb7dd2
amahiner7 <amahiner@gmail.com>
2021-10-22 19:03:38 +0900

    evaluate_sentence / translate 구현

--- model/ver1/Transformer.py
+++ model/ver1/Transformer.py
@@ -15,7 +15,6 @@ from utils.CosineAnnealingWarmUpRestarts import CosineAnnealingWarmUpRestarts
 from utils.LearningRateHistory import LearningRateHistory
 from utils.CustomSchedule import CustomSchedule
 
-
 train_step_signature = [tf.TensorSpec(shape=(None, None), dtype=tf.int64),
                         tf.TensorSpec(shape=(None, None), dtype=tf.int64)]
 
@@ -239,6 +238,56 @@ class Transformer(Model):
     #                      self.valid_metric_accuracy.result() * 100.0,
     #                      math.exp(self.valid_metric_loss.result()),
     #                      format_time(time.time() - train_start_time)))
+
+    def train_on_epoch(self, train_data, valid_data, epochs, callbacks, verbose):
+        self.training = True
+        history = self.fit(train_data,
+                           validation_data=valid_data,
+                           epochs=epochs,
+                           callbacks=callbacks,
+                           verbose=verbose)
+
+        return history
+
+    def evaluate_sentence(self, sentence, tokenizer_pt, tokenizer_en, max_seq_len):
+        self.training = False
+        start_token = [tokenizer_pt.vocab_size]
+        end_token = [tokenizer_pt.vocab_size + 1]
+
+        # 입력 문장은 포르투갈어이므로 start 토큰과 end 토큰을 추가합니다.
+        sentence = start_token + tokenizer_pt.encode(sentence) + end_token
+        encoder_input = tf.expand_dims(sentence, 0)
+
+        # 타겟은 영어이므로 트랜스포머의 첫번째 단어는 영어 start 토큰입니다.
+        decoder_input = [tokenizer_en.vocab_size]
+        output = tf.expand_dims(decoder_input, 0)
+
+        for i in range(max_seq_len):
+            # predictions.shape == (batch_size, seq_len, vocab_size)
+            predictions, attention_weights = self.call(encoder_input, output)
+
+            # seq_len 차원에서 마지막 단어를 선택합니다.
+            predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
+
+            predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+            # predicted_id가 end 토큰과 같으면 결과를 반환합니다.
+            if predicted_id == tokenizer_en.vocab_size + 1:
+                return tf.squeeze(output, axis=0), attention_weights
+
+            # predicted_id를 디코더의 입력값으로 들어가는 출력값과 연결합니다.
+            output = tf.concat([output, predicted_id], axis=-1)
+
+        return tf.squeeze(output, axis=0), attention_weights
+
+    def translate(self, sentence, tokenizer_en):
+        result, attention_weights = self.evaluate_sentence(sentence)
+
+        predicted_sentence = tokenizer_en.decode([i for i in result
+                                                  if i < tokenizer_en.vocab_size])
+
+        print('Input: {}'.format(sentence))
+        print('Predicted translation: {}'.format(predicted_sentence))
 
     def compile_model(self):
         self.compile(optimizer=self.optimizer, loss=self.criterion, metrics=['accuracy'])
