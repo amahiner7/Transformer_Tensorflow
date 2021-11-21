bc9d22503cc1138705988e1f76efd84609075f1a
amahiner7 <amahiner@gmail.com>
2021-11-21 23:58:19 +0900

    1. HyperParameter 클래스 ㅊ우가 2. argparse 적용

--- run_train_ver1.py
+++ run_train_ver1.py
@@ -1,26 +1,35 @@
-from config.hyper_parameters import *
+import argparse
+from config.hyper_parameters import HyperParameter
 from data.legacy.load_data import load_data
 from model.ver1.Transformer import Transformer
 from utils.common import *
 
-train_data_loader, valid_data_loader = load_data()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Train transformer model")
+    parser.add_argument("--epochs", help="Training epochs", type=int,
+                        required=False, default=HyperParameter.NUM_EPOCHS, metavar="20")
 
-model = Transformer(d_input=train_data_loader.encoder_vocab_size,
-                    d_output=train_data_loader.decoder_vocab_size,
-                    d_embed=EMBED_DIM,
-                    d_model=MODEL_DIM,
-                    d_ff=FF_DIM,
-                    num_heads=NUM_HEADS,
-                    num_layers=NUM_LAYERS,
-                    dropout_prob=DROPOUT_PROB,
-                    seq_len=MAX_SEQ_LEN)
-model.summary_model()
+    args = parser.parse_args()
+    epochs = args.epochs
 
-model.compile_model()
-history = model.train_on_epoch(train_data=train_data_loader.item,
-                               valid_data=valid_data_loader.item,
-                               epochs=NUM_EPOCHS,
-                               callbacks=model.make_callbacks(),
-                               verbose=1)
+    train_data_loader, valid_data_loader = load_data()
 
-display_loss(history.history)
+    model = Transformer(d_input=train_data_loader.encoder_vocab_size,
+                        d_output=train_data_loader.decoder_vocab_size,
+                        d_embed=HyperParameter.EMBED_DIM,
+                        d_model=HyperParameter.MODEL_DIM,
+                        d_ff=HyperParameter.FF_DIM,
+                        num_heads=HyperParameter.NUM_HEADS,
+                        num_layers=HyperParameter.NUM_LAYERS,
+                        dropout_prob=HyperParameter.DROPOUT_PROB,
+                        seq_len=HyperParameter.MAX_SEQ_LEN)
+    model.summary_model()
+
+    model.compile_model()
+    history = model.train_on_epoch(train_data=train_data_loader.item,
+                                   valid_data=valid_data_loader.item,
+                                   epochs=epochs,
+                                   callbacks=model.make_callbacks(),
+                                   verbose=1)
+
+    display_loss(history.history)
